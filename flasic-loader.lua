a5380017f515627529c461f0c5417c06d7331df4
10GoodDogs <89498174+10GoodDogs@users.noreply.github.com>
2022-10-21 21:51:47 -0700

    Create flasic-loader.lua

---
+++ flasic-loader.lua
@@ -0,0 +1,11 @@
+local games = {
+  {id = 0, url="SPTS"}
+}
+
+-- this shit is obfuscated so dont you dare skid it
+local url = 'https://scripts.luawl.com/%s/Flasic.lua'
+for _,v in pairs(games) do
+    if v.id == game.PlaceId then
+      loadstring(game:HttpGet(string.format(url, v.url)))
+    end
+end
